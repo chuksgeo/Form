5d36ae494e803cc4567d514502f698a721817946
George Chukwuebuka <ebuka.g@zyonel.com>
2022-11-09 12:16:42 +0100

    updated

--- user_data.php
+++ user_data.php
@@ -1,20 +1,18 @@
 <?php
 
-$name = "Name: ".$_POST['firstname']." ".$_POST['lastname'];
-$gender = "Gender: ".$_POST['gender'];
-$email = "Email: ".$_POST['email'];
-$dob = "Date of birth: ".$_POST['dob'];
+    $data['name'] = $_POST['firstname']." ".$_POST['lastname'];
+    $data['gender'] = $_POST['gender'];
+    $data['email'] = $_POST['email'];
+    $data['dob'] = $_POST['dob'];
 
-$filename = './userdata.csv'; 
-$handle = fopen($filename, "a");    
-$content = fwrite($handle, $name.",");
-$content = fwrite($handle, $gender.",");
-$content = fwrite($handle, $email.",");
-$content = fwrite($handle, $dob.",");
-fclose($handle);
 
-$filename = './userdata.csv'; 
-$handle = fopen($filename, "r"); 
-$content = fread($handle, filesize($filename));
-print_r($content);
-fclose($handle);
+    $filename = './userdata.csv'; 
+    $handle = fopen($filename, "a");    
+    $content = fwrite($handle, implode(",", $date));
+    fclose($handle);
+
+    $filename = './userdata.csv'; 
+    $handle = fopen($filename, "r"); 
+    $content = fread($handle, filesize($filename));
+    print_r($content);
+    fclose($handle);
